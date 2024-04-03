497cf662d1c723f9480b0a1a5a68864fc800f3b3
Samandar Baratov <sabaratov9999@gmail.com>
2024-04-03 18:03:21 +0500

    more Improved

--- ParkingZoneApp/ViewModels/ParkingZones/ListItemVM.cs
+++ ParkingZoneApp/ViewModels/ParkingZones/ListItemVM.cs
@@ -8,16 +8,5 @@ namespace ParkingZoneApp.ViewModels.ParkingZones
         public string Name { get; set; }
         public string Address { get; set; }
         public DateTime DateOfEstablishment { get; set; }
-
-        public ListItemVM MapToVM(ParkingZone parkingZone)
-        {
-            return new ListItemVM
-            {
-                Id = parkingZone.Id,
-                Name = parkingZone.Name,
-                Address = parkingZone.Address,
-                DateOfEstablishment = parkingZone.DateOfEstablishment
-            };
-        }
     }
 }
