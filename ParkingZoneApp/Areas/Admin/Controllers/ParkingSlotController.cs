9aee0583eff46c1e5ed331f88a71f145fe50e0c9
Samandar Baratov <sabaratov9999@gmail.com>
2024-04-27 00:03:09 +0500

    dunno

--- ParkingZoneApp/Areas/Admin/Controllers/ParkingSlotController.cs
+++ ParkingZoneApp/Areas/Admin/Controllers/ParkingSlotController.cs
@@ -1,7 +1,5 @@
 ﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
-using ParkingZoneApp.Models;
 using ParkingZoneApp.Services;
 using ParkingZoneApp.ViewModels.ParkingSlotVMs;
 
