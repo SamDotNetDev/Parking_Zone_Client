b8fc1b990d88d1eb89952eb928f672761bdb6a6f
Samandar Baratov <sabaratov9999@gmail.com>
2024-05-20 16:11:00 +0500

    requested changes are done

--- ParkingZoneApp/Areas/Admin/Controllers/ParkingSlotController.cs
+++ ParkingZoneApp/Areas/Admin/Controllers/ParkingSlotController.cs
@@ -20,15 +20,17 @@ namespace ParkingZoneApp.Areas.Admin
 
         public IActionResult Index(int ParkingZoneId)
         {
+            var zone = _zoneService.GetById(ParkingZoneId);
             var VMs = _slotService.GetByParkingZoneId(ParkingZoneId)
                 .Select(x => new ListItemVM(x))
                 .OrderBy(x => x.Number);
-            if(!VMs.Any())
+
+            if (zone is null)
             {
                 return NotFound();
             }
 
-            ViewData["Name"] = _zoneService.GetById(ParkingZoneId).Name;
+            ViewData["Name"] = zone.Name;
             ViewData["ParkingZoneId"] = ParkingZoneId;
             return View(VMs);
         }
