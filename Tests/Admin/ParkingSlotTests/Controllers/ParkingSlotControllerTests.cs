b9da7b70085b5401f4d5a49b8d8ad3b4ab27ce5f
Samandar Baratov <sabaratov9999@gmail.com>
2024-05-20 16:53:55 +0500

    filtering added

--- Tests/Admin/ParkingSlotTests/Controllers/ParkingSlotControllerTests.cs
+++ Tests/Admin/ParkingSlotTests/Controllers/ParkingSlotControllerTests.cs
@@ -42,6 +42,7 @@ namespace Tests.Admin.ParkingSlotTests.Controllers
             //Arrange
             var expectedParkingSlots = new List<ParkingSlot>() { _parkingSlotsTest };
 
+            _zoneService.Setup(x => x.GetById(Id));
             _slotService.Setup(x => x.GetByParkingZoneId(Id));
 
             //Act
@@ -51,6 +52,7 @@ namespace Tests.Admin.ParkingSlotTests.Controllers
             Assert.NotNull(result);
             Assert.IsType<NotFoundResult>(result);
             _slotService.Verify(x => x.GetByParkingZoneId(Id), Times.Once);
+            _zoneService.Verify(x => x.GetById(Id), Times.Once);
         }
 
         [Fact]
