a169beb824fc3b018f52d6c2efc4abddf0f7f6b3
Samandar Baratov <sabaratov9999@gmail.com>
2024-05-29 00:32:11 +0500

    SuperAdmin Role Added

---
+++ ParkingZoneApp/Enums/RolesEnum.cs
@@ -0,0 +1,27 @@
+﻿namespace ParkingZoneApp.Enums
+{
+    public enum RolesEnum
+    {
+        User = 1,
+        Admin,
+        SuperAdmin
+    }
+
+    public class RoleConverter
+    {
+        public RolesEnum ConvertToUserRole(string roleName)
+        {
+            switch (roleName)
+            {
+                case "SuperAdmin":
+                    return RolesEnum.SuperAdmin;
+                case "Admin":
+                    return RolesEnum.Admin;
+                case "User":
+                    return RolesEnum.User;
+                default:
+                    throw new ArgumentException($"Unknown role: {roleName}");
+            }
+        }
+    }
+}
---
+++ ParkingZoneApp/ViewModels/SuperAdminVMs/ListItemVM.cs
@@ -0,0 +1,28 @@
+﻿using ParkingZoneApp.Models;
+
+namespace ParkingZoneApp.ViewModels.SuperAdminVMs
+{
+    public class ListItemVM
+    {
+        public string Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string UserName { get; set; }
+
+        public string PhoneNumber { get; set; }
+
+        public IList<string> Role { get; set; }
+
+        public ListItemVM(ApplicationUser user)
+        {
+            Id = user.Id;
+            Name = user.Name;
+            UserName = user.UserName;
+            PhoneNumber = user.PhoneNumber;
+            Role = new List<string>();
+        }
+
+        public ListItemVM() { }
+    }
+}
